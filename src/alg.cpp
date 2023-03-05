4f49f1748d069034f29e1c1933949f77347167ee
DimNekor <117307225+DimNekor@users.noreply.github.com>
2023-03-05 20:39:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int res = 0;
+  int start = 0;
+  int end = size - 1;
+  while (start <= end) {
+    int middle = (start + end) / 2;
+    if (*(arr + middle) == value) {
+      res += 1;
+      int middle1 = middle;
+      while ((*(arr + middle + 1)) == value) {
+        res += 1;
+        middle += 1;
+      }
+      while ((*(arr + middle1 - 1)) == value) {
+        res += 1;
+        middle1 -= 1;
+      }
+      return res;
+    }
+    else if (*(arr + middle) > value) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+  return res;
 }
